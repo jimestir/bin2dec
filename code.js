6a061ee39817406b5cd9b95de545b18c5a1426f9
jimestir <jimmyestir@gmail.com>
2022-08-13 16:25:27 +0200

    Fix validations

--- code.js
+++ code.js
@@ -1,31 +1,45 @@
 const resultDiv = document.getElementById("result");
 let response = document.querySelector(".response");
 let modal = document.querySelector(".modal");
-const r = /[^0-1]/g;
+const r = /[^01]/g;
 let bin = null;
 let result = 0;
 let j = 0;
-function bin2dic() {
-  bin = document.getElementById("input").value;
-  if (bin.length === 0) return;
-
-  if (bin.length > 9) {
-    document.getElementById("input").value = "";
-    modal.classList.toggle("visible");
-    return (response.innerHTML = "Cannot be more than eight digits supplied");
-  } else {
-    bin = "0".repeat(9 - bin.length) + bin;
+let prevBin = 0;
+const DIGIT__BIN = 8;
+function validationBin(bin) {
+  if (prevBin === bin) {
+    prevBin = bin;
+    return false;
   }
+  prevBin = bin;
   if (r.test(bin)) {
-    document.getElementById("input").value = "";
-    modal.classList.toggle("visible");
-    return (response.innerHTML = "Invalid number can only be supplied 0 and 1");
-  }
+    response.innerHTML = "Invalid number can only be supplied 0 and 1";
+    return false;
+  } else if (bin.length > DIGIT__BIN) {
+    response.innerHTML = "Cannot be more than eight digits supplied";
+    return false;
+  } else return true;
+}
+
+function bin2dic(e) {
+  e.preventDefault();
+  bin = document.getElementById("input").value;
+  bin.length < DIGIT__BIN
+    ? (bin = "0".repeat(DIGIT__BIN - bin.length) + bin)
+    : "";
   result = 0;
   j = 0;
-  for (let i = 8; i >= 0; i--) {
+  for (let i = DIGIT__BIN; i > 0; i--) {
     result += parseInt(bin[j]) * Math.pow(2, i);
     j++;
   }
-  return (resultDiv.innerText = result);
+
+  if (validationBin(bin)) {
+    document.getElementById("input").value = "";
+    return (resultDiv.innerText = result);
+  } else {
+    document.getElementById("input").value = "";
+    modal.classList.toggle("visible");
+  }
 }
